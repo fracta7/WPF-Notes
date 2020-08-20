14d6de5d1457de748fb60d90dfe45d93a423444a
Javokhir Matnazarov <fracta7@gmail.com>
2020-08-20 17:07:50 +0500

    Add project files.

---
+++ WPF Notes/Colors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Notes
+{
+    //class for storing colors for theming
+    public static class Colors
+    {
+        //dark theme color palletes
+        public static string DarkBGPrimary = "#222831";
+        public static string DarkBlueSecondary = "#30475e";
+        public static string WhiteText = "#ececec";
+
+        //light theme colors
+        public static string WhiteBGPrimary = "#ececec";
+        public static string AquaButton = "#318fb5";
+        public static string DarkText = "#050505";
+    }
+}
